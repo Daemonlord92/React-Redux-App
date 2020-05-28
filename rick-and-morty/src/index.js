4f52c85b628305d644f7b6b6815f6e274b4a3629
Matthew Martin <zhintateo19924559@gmail.com>
2020-05-28 14:41:09 -0500

    Completed Actions

--- rick-and-morty/src/index.js
+++ rick-and-morty/src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from "redux"
+import thunk from 'redux-thunk'
+
+import { reducer } from './'
+
+const store = createStore();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
